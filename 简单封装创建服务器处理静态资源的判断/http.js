6f7722b67d915f0089c215d2b60d3bcfecf749d6
假的假的假假的 <jiadejiadejiajiade@qq.com>
2019-07-27 23:47:44 +0800

    简单封装创建服务器及处理静态资源的判断

---
+++ 简单封装创建服务器处理静态资源的判断/http.js
@@ -0,0 +1,26 @@
+﻿//引用http模块
+const http = require('http');
+const fs = require('fs');
+//创建服务器
+const server = http.createServer();
+//绑定端口及ip
+server.listen(8080, '127.0.0.1', () => {
+    console.log('服务器开启');
+} )
+//注册事件
+server.on('request', (req, res) => {
+    //链接
+    if(req.url.startsWith('/views') || req.url.startsWith('/css') || req.url.startsWith('/images')){
+        //是否是css
+        if(req.url.endsWith('.css')){
+            //加请求头
+            res.setHeader('Content-Type', 'text/css');
+        }
+
+        //文件
+        fs.readFile('../' + req.url, (err, data) => {
+            if(err) throw err;
+            res.end(data);
+        } )
+    }
+} )
